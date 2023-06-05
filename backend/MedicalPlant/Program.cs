8d3f9297e2cdc50bd6a011d649a348f5ff4b7c29
Mahdi <74624149+zoubirabde9a@users.noreply.github.com>
2023-06-05 19:49:58 +0100

    add cors

--- backend/MedicalPlant/Program.cs
+++ backend/MedicalPlant/Program.cs
@@ -14,12 +14,20 @@ builder.Services.AddDbContext<DataContext>(options =>
     options.UseNpgsql(connectionString);
 });
 
+builder.Services.AddCors();
+
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseCors(options =>
+    options.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
